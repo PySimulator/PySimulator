d25492b585c1c89372bbaafeb8d76518e62c6f2c
tbeu <tc@tbeu.de>
2014-03-21 09:22:03 +0100

    added exception handling when loading SimulationResult plugins

--- PySimulator/Plugins/SimulationResult/__init__.py
+++ PySimulator/Plugins/SimulationResult/__init__.py
@@ -7,12 +7,21 @@ def get_immediate_subdirectories(directory):
 PlugInNames = get_immediate_subdirectories(os.path.abspath(os.path.dirname(__file__)))
 plugin = []
 for i in range(len(PlugInNames)):
-    mod = __import__(PlugInNames[i] + "." + PlugInNames[i], locals(), globals(), [PlugInNames[i] + "." + PlugInNames[i]])
-    plugin.append(mod)
-
+    try:
+        mod = __import__(PlugInNames[i] + "." + PlugInNames[i], locals(), globals(), [PlugInNames[i] + "." + PlugInNames[i]])
+        plugin.append(mod)
+    except ImportError as e:
+        print PlugInNames[i] + " plug-in could not be loaded. Error message: '" + e.message + "'"
+    except SyntaxError as e:
+        print PlugInNames[i] + " plug-in could not be loaded. Error message: '" + str(e) + "'"
+    except Exception as e:
+        info = str(e)
+        if info == '' or info is None:
+            print PlugInNames[i] + " plug-in could not be loaded."
+        else:
+            print PlugInNames[i] + " plug-in could not be loaded. Error message: '" + info + "'"
 fileExtension = []
 description = []
 for p in plugin:
     fileExtension.append(p.fileExtension)
     description.append(p.description)
-
