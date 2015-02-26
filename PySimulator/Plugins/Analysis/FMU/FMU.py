6302f4027145e0788a4f909d3fc39b18332a3561
arun3688 <rain100falls@gmail.com>
2015-02-26 10:27:05 +0100

    Add connections to Table From xmlsetup

--- PySimulator/Plugins/Analysis/FMU/FMU.py
+++ PySimulator/Plugins/Analysis/FMU/FMU.py
@@ -391,18 +391,42 @@ class ConnectFMUsDialog(QtGui.QDialog):
              self.xmlFileTextBox.setText(setupfile)
              tree = ET.parse(setupfile)
              root = tree.getroot()
+             
+             ## Add fmu's to list with correct instance from xmlsetup
              for fmu in root.iter('fmu'):
                  name = fmu.get('path')
                  fmuinstance=fmu.get('instanceName')
                  self._fmusListModel.addFMUfromXMLsetup(name,fmuinstance)
-                        
-             ''' 
+                     
+             ## Add connection information to table from xmlsetup 
              for connection in root.iter('connection'):
-                fid=connection.get('fromInstanceName')
-                fvar=connection.get('fromVariableName')
-                tid=connection.get('fromInstanceName')'''
+                frominstance=connection.get('fromInstanceName')
+                fromvar=connection.get('fromVariableName')
+                toinstance=connection.get('toInstanceName')
+                tovar=connection.get('toVariableName')
                 
-                 
+                for i in xrange(len(self._fmusListModel._fmus)):
+                      instance=self._fmusListModel._fmus[i]._instanceName
+                      
+                      if (instance==frominstance):
+                           fromFMU=self._fmusListModel._fmus[i]
+                           frominputoutputvar=self._fmusListModel._fmus[i]._inputsOutputs
+                           for j in xrange(len(frominputoutputvar)):
+                                varname=frominputoutputvar[j]['name']
+                                if(fromvar==varname):
+                                      inputVar=frominputoutputvar[j]  
+                                      
+                      if (instance==toinstance):
+                           toFMU=self._fmusListModel._fmus[i]
+                           toinputoutputvar=self._fmusListModel._fmus[i]._inputsOutputs
+                           for j in xrange(len(toinputoutputvar)):
+                                varname=toinputoutputvar[j]['name']                               
+                                if(tovar==varname):
+                                      outputVar=toinputoutputvar[j]                                    
+                              
+                self._connectionsListModel.addConnection(fromFMU, inputVar, toFMU, outputVar)
+                self._connectionsTableView.resizeColumnsToContents()
+
     def browseFmuFile(self):
         (fileNames, trash) = QtGui.QFileDialog().getOpenFileNames(self, 'Open File', os.getcwd(), '(*.fmu)')
         for fileName in fileNames:
