f41099c0eaac625e6bf0358e74421708ffccd5d0
arun3688 <rain100falls@gmail.com>
2015-04-29 13:09:45 +0200

    Minor fix to Regression report- calculate percent

--- PySimulator/Plugins/Analysis/Testing/Testing.py
+++ PySimulator/Plugins/Analysis/Testing/Testing.py
@@ -236,7 +236,7 @@ def htmloverview(fileouthtml,resultfile,file,file1,diff1,difftol,dircount,model1
     messerr="""<html> <head> <h2> Sorted by Name </h2> </head> <table> <tr> <th> Name </th> <th> Estimated Tolerance </th> </td> """  
     messtol="""<html> <head> <h2> Sorted by Highest Tolerance Error </h2> </head> <table> <tr> <th> Name </th> <th> Estimated Tolerance </th> </td> """  
     
-    message1= '<a href=' + os.path.relpath(resultfile) + '>' + modelname +'.'+ model1var+'</a>' +' </td>' 
+    message1= '<a href=' + os.path.relpath(resultfile) + '>' + modelname +'-'+ model1var+'</a>' +' </td>' 
     if(len(diff1)==0):
          #emptyhref='<a href="" style="text-decoration:none;">0'+ '(' + str(totalvar) + 'variables)' +'</a>'
          emptyhref='<a href="" style="text-decoration:none;">'+ model2var+'/'+ str(totalComparedvar) +'[' +str(maxEstTol)+ ']' +'</a>'
@@ -877,15 +877,14 @@ class simulationParallelThread(QtCore.QThread):
     
             #check the subdirectory for empty strings and replace it with 'N' for passing to pool.map(), as it cannot process empty list of strings to multiprocess module
             subdirlist= ["N" if not x else x for x in self.modelList['subDirectory']]
-            
             ## Create a Pool of process and run the Simulation in Parallel
             pool=Pool()
-            #startTime = time.time() 
+            startTime = time.time() 
             pool.map(ParallelSimulation, zip(self.modelList['fileName'],self.modelList['modelName'],subdirlist,self.modelList['tStart'],self.modelList['tStop'],self.modelList['tol'],self.modelList['stepSize'],self.modelList['nInterval'],self.modelList['includeEvents'],dirs,resultpath,config,simname))
             pool.close()
             pool.join()
-            #elapsedTime = time.time() - startTime
-            #print elapsedTime           
+            elapsedTime = time.time() - startTime
+            print elapsedTime         
         print "Parallel simulation completed"
         self.running = False
             
@@ -979,6 +978,7 @@ def ParallelSimulation(modellists):
        import traceback
        traceback.print_exc(e,file=sys.stderr)
        print e
+       
 
 
 class simulationThread(QtCore.QThread):
@@ -1044,6 +1044,7 @@ class simulationThread(QtCore.QThread):
                     haveCOM = True
                 except:
                     pass
+                startTime=time.time() 
                 for i in xrange(len(self.modelList['fileName'])):
                     if self.stopRequest:
                         print "... Simulations canceled."
@@ -1113,7 +1114,8 @@ class simulationThread(QtCore.QThread):
                         pythoncom.CoUninitialize()  # Close the COM library on the current thread
                     except:
                         pass
-
+        elapsedTime = time.time() - startTime
+        print elapsedTime   
         print "... running the list of simulations done."
         self.running = False
         
@@ -1665,9 +1667,11 @@ def genregressionreport(logfile,totaldir,filecount,Time,resultdirsize,baselinedi
         failfiles=[]
         for z in xrange(len(hname)):
           d=str(hname[z].string).split('/')
-          passfiles.append(int(d[0][0]))
-          failfiles.append(int(d[1][0])) 
-         
+          passednumber=str(d[0]).split('passed')
+          failednumber=str(d[1]).split('failed')
+          passfiles.append(int(passednumber[0]))
+          failfiles.append(int(failednumber[0])) 
+               
         p1=int(sum(passfiles))+int(sum(failfiles))
         p2=int(sum(passfiles))*100/p1
         pstatus.append(str(p2))
@@ -1707,10 +1711,10 @@ def genregressionreport(logfile,totaldir,filecount,Time,resultdirsize,baselinedi
       if(i%2==0):
          x=get_column(i,hreflist)
          x1=x[0].find('a').string
-         y=x1.split('.')
+         y=x1.split('-')
          #href='<a href='+os.path.basename(logfile)+'>'+ x1 +'</a>'         
          #s='\n'.join(['<tr>','<td>',href,'</td>'])
-         s='\n'.join(['<tr>','<td>',str(y[0]),'</td>','<td id=status>','</td>','<td>',str(y[1]),'</td>'])
+         s='\n'.join(['<tr>','<td>',str(y[0]),'</td>','<td id=status>','</td>','<td>',str(y[-1]),'</td>'])
          f.write(s)
          f.write('\n')
          
